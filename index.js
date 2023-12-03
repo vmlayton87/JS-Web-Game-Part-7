c45f59c446b8adb3f694e0bad231390b8081a7c4
vmlayton87 <vmlayton87@gmail.com>
2023-12-03 02:43:33 -0500

    solution no bonus

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
+// npc path
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
+moveNPC()
+
 
 // Create the inventory
 const inventory = newInventory()
